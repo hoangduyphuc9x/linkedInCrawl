443e6b3565b3bfc116e80f0392f0c5c41958926a
Hoàng Duy Phúc <hoangduyphuc9x@gmail.com>
2021-11-23 01:07:15 +0700

    new milk

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+var jjj = "milk"
 var mongooseSet = require("./mongooseSet");
 
 // view engine setup
@@ -20,7 +21,7 @@ app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.io = io;
   next();
 });
@@ -34,7 +35,7 @@ app.use("/", routes);
 // app.use("/users", users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
@@ -45,7 +46,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     console.log(err);
     res.status(err.status || 500);
     res.render("error", {
@@ -57,7 +58,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.log(err);
   res.status(err.status || 500);
   res.render("error", {
